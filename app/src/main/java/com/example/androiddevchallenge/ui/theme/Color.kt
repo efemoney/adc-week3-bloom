4c3c0a072e4ff3849164ffed80be6a024eb82a76
Efeturi Money <efeturi.money@gmail.com>
2021-03-13 03:44:38 +0100

    Add colors

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Color.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Color.kt
@@ -21,3 +21,12 @@ val purple200 = Color(0xFFBB86FC)
 val purple500 = Color(0xFF6200EE)
 val purple700 = Color(0xFF3700B3)
 val teal200 = Color(0xFF03DAC5)
+
+inline val white get() = Color.White
+val white150 = white.copy(alpha = 0.15f)
+val white850 = white.copy(alpha = 0.85f)
+val pink100 = Color(0xfffff1f1)
+val pink900 = Color(0xff3f2c2c)
+val grey = Color(0xff232323)
+val green900 = Color(0xff2d3b2d)
+val green300 = Color(0xffb8c9b8)
